372d19fce1610cca451e1f741740dfcc70ed0dce
jokatty <jyotikattani@gmail.com>
2021-09-14 21:07:44 +0800

    calculate container size

--- src/components/DisplayItems.jsx
+++ src/components/DisplayItems.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
@@ -11,49 +12,47 @@ export default function DisplayItems({ items }) {
   // global state
   // 8ft (2.43m) wide, 8.5ft (2.59m) high
   // 20ft (6.06m) and 40ft (12.2m).
-  const [totalLength, setTotalLength] = useState(0);
-  const [totalWidth, setTotalWidth] = useState(0);
-  const [totalHeight, setTotalHeight] = useState(0);
+  let [totalVolume, setTotalVolume] = useState(0);
 
   function handleAddClick() {
-    setTotalLength(Number(totalLength) + (Number(length)));
-    console.log(totalLength);
-    setLength(0);
-    setTotalWidth(Number(totalWidth) + Number(width));
-    console.log(totalWidth);
-    setWidth(0);
-    setTotalHeight(Number(totalHeight) + Number(height));
-    console.log(totalHeight);
-    setHeight(0);
+    const cuboidSide = Math.max(length, width, height);
+    setTotalVolume(totalVolume + (cuboidSide ** 3));
   }
   // calculate the container size
   function handleCalcClick() {
     let numOfFortyFtContainers = 0;
     let numOfTwentyFtContainer = 0;
-    let length = totalLength;
-    while (length > 0) {
-      if (length > 40) {
-        length -= 40;
+    const volOf20FtContainer = 20 * 8 * 8.5;
+    const volOf40FtContainer = 40 * 8 * 8.5;
+    while (totalVolume > 0) {
+      console.log('while loop runnin');
+      if (totalVolume > volOf40FtContainer) {
         numOfFortyFtContainers += 1;
+        totalVolume -= volOf40FtContainer;
       }
-      if (length > 20 && length <= 40) {
-        length -= Math.round(length / 40);
-        numOfFortyFtContainers += 1;
-      }
-      if (length <= 20) {
-        length -= Math.round(length / 20);
+      else if (totalVolume > volOf20FtContainer && totalVolume < volOf40FtContainer) {
         numOfTwentyFtContainer += 1;
+        totalVolume -= volOf20FtContainer;
+      }
+      else if (totalVolume <= volOf20FtContainer) {
+        numOfTwentyFtContainer += 1;
+        totalVolume -= volOf20FtContainer;
       }
     }
     let message = '';
     if (numOfFortyFtContainers >= 1) {
-      message += `You need ${numOfFortyFtContainers} 40ft container`;
+      message += `You need ${numOfFortyFtContainers}, 40ft.`;
     }
     if (numOfTwentyFtContainer >= 1) {
-      message += `and ${numOfTwentyFtContainer} 20ft containers`;
+      message += `${numOfTwentyFtContainer}, 20ft containers.`;
     }
     setResultMessage(message);
+    setTotalVolume(0);
+    setLength(0);
+    setWidth(0);
+    setHeight(0);
   }
+
   return (
     <>
       {items.map((item) => (
@@ -69,7 +68,11 @@ export default function DisplayItems({ items }) {
         </>
       ))}
       <button type="button" onClick={handleCalcClick}>Calculate container size</button>
-      {resultMessage !== '' && <p>{resultMessage}</p>}
+      {resultMessage !== '' && (
+      <p>
+        {resultMessage}
+      </p>
+      )}
     </>
   );
 }
