c931f1eaacbab76bd8d9dc65d25b34e66f4b9d16
jokatty <jyotikattani@gmail.com>
2021-09-17 20:49:02 +0800

    add logic for multiple image upload

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import DisplayItems from './DisplayItems.jsx';
+import { addItemsAction, ImageContext } from '../store.js';
 // make sure that axios always sends the cookies to the backend server
 axios.defaults.withCredentials = true;
 const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
 console.log(REACT_APP_BACKEND_URL);
 
 export default function UploadImage() {
+  const { dispatch } = useContext(ImageContext);
   const [selectedFile, setSelectedFile] = useState('');
   const [items, setItems] = useState([]);
-  const [uploadedImage, setUploadedImage] = useState('');
+  const [uploadedImage, setUploadedImage] = useState([]);
   async function readImages(e) {
     const image = e.target.files[0];
     setSelectedFile(image);
@@ -20,11 +22,13 @@ export default function UploadImage() {
     const formData = new FormData();
     formData.append('image', selectedFile);
     console.log(formData);
-    setUploadedImage(URL.createObjectURL(selectedFile));
-    console.log(uploadedImage);
+    const localImageUrl = URL.createObjectURL(selectedFile);
+    setUploadedImage([...uploadedImage, localImageUrl]);
+    console.log('uploaded image', uploadedImage);
     const postedData = await axios.post('http://localhost:3004/upload', formData);
     console.log(postedData.data);
-    setItems(postedData.data);
+    setItems([...postedData.data]);
+    dispatch(addItemsAction(postedData.data));
   }
 
   return (
@@ -32,7 +36,9 @@ export default function UploadImage() {
       <input type="file" onChange={readImages} />
       <button type="button" onClick={handleClick}>upload</button>
       <br />
-      {uploadedImage !== '' && <img src={uploadedImage} alt="uploadedImage" />}
+      {uploadedImage.length !== 0 && uploadedImage.map((image) => (
+        <img src={image} alt="uploadedImage" />
+      ))}
       {items.length !== 0 && <DisplayItems items={items} />}
     </>
   );
